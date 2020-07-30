a1bb5d3573eb95e101e5e79711600cad3fa0b9e9
rafaelmdalmoro <rafamascmoro@hotmail.com>
2020-07-30 17:26:48 -0300

    Versao simples
    Criação da versão simplificada da biblioteca, apenas substituindo os termos pelas traduções

---
+++ src/VersaoAlternativa/NFC.h
@@ -0,0 +1,38 @@
+/**
+ * Biblioteca Arduino para modulo MFRC522.
+ * 
+ * @autores Rafael Mascarenhas  e Grabriel Pacheco.
+ * 
+ * Para mais informacoes , leia o documento README.
+ * 
+ * Por favor, leia este arquivo para uma visão geral da biblioteca e, depois, o arquivo .cpp para especificacoe das funcoes
+ */
+#ifndef NFC_h
+#define NFC_h
+
+#include "mfrc522.h"
+#include <Arduino.h>
+#include <SPI.h>
+
+class NFC{
+  public: 
+    NFC();
+	NFC(byte pinoReset);
+	NFC(byte pinoSelecaoChip, byte pinoReset);
+    
+	void iniciar();
+	void iniciar(byte pinoReset);
+	void iniciar(byte pinoSelecaoChip, byte pinoReset);
+    	
+    virtual bool CartaoEstaPresente();
+	virtual bool LerCodigoCartao();
+	
+	String imprimirUID();
+	
+  protected:
+    byte _pinoSelecaoChip, _pinoReset;		
+};
+
+
+
+#endif
